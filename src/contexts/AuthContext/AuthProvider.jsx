56c7f9dd4425c8d279b5828be040de8fe9d5d8f3
Suronjit Sutradhar <suronjit02@gmail.com>
2026-08-22 02:34:31 +0600

    feat: login from firebase user, login with googlePopup

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -2,20 +2,34 @@ import React from "react";
 import { AuthContext } from "./AuthContext";
 import { createUserWithEmailAndPassword } from "firebase/auth/cordova";
 import { auth } from "../../firebase/firebase.init";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
+  // create user
   const registerUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  // login user
   const signInUser = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // social login: Google
+  const googleSignIn = () => {
+    return signInWithPopup(auth, googleProvider);
   };
 
   const authInfo = {
     registerUser,
     signInUser,
+    googleSignIn,
   };
 
   return <AuthContext value={authInfo}>{children}</AuthContext>;
