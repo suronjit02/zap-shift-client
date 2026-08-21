bf54b01f6e7bb7aebca0a332123dbfcdc000bc34
Suronjit Sutradhar <suronjit02@gmail.com>
2026-08-22 01:30:49 +0600

    feat: firebase user create complete

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router";
+import useAuth from "../../hooks/useAuth";
 
 const Register = () => {
   const {
@@ -9,8 +10,18 @@ const Register = () => {
     formState: { errors },
   } = useForm();
 
+  const { registerUser } = useAuth();
+
   const handleRegistration = (data) => {
     console.log(data);
+
+    registerUser(data.email, data.password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
