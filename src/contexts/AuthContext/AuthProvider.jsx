fe04352b94bd18126e4e989bc72a47fd34cda411
Suronjit Sutradhar <suronjit02@gmail.com>
2026-08-22 23:52:13 +0600

    manage and observe user state and loading state

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,35 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import { createUserWithEmailAndPassword } from "firebase/auth/cordova";
 import { auth } from "../../firebase/firebase.init";
 import {
   GoogleAuthProvider,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signInWithPopup,
+  signOut,
 } from "firebase/auth";
 
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   // create user
   const registerUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   // login user
   const signInUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   // social login: Google
   const googleSignIn = () => {
+    setLoading(true);
     return signInWithPopup(auth, googleProvider);
   };
 
+  // logout
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // observe user state
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
   const authInfo = {
+    user,
+    loading,
     registerUser,
     signInUser,
     googleSignIn,
+    logOut,
   };
 
   return <AuthContext value={authInfo}>{children}</AuthContext>;
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router";
 import useAuth from "../../hooks/useAuth";
+import SocialLogin from "./SocialLogin/SocialLogin";
 
 const Register = () => {
   const {
